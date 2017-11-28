cd58e3973de385b60de23cb070d0c9c373850f0f
lyztxyd1995 <31735325+lyztxyd1995@users.noreply.github.com>
2017-11-28 00:20:13 -0500

    Add files via upload

---
+++ around/src/Register.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import $ from 'jquery';
+import { API_ROOT } from './constants';
+import { Form, Input, Button, message } from 'antd';
+const FormItem = Form.Item;
+
+class RegistrationForm extends React.Component {
+    state = {
+        confirmDirty: false,
+        autoCompleteResult: [],
+    };
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+                $.ajax({
+                    url: `${API_ROOT}/signup`,
+                    method: 'POST',
+                    data: JSON.stringify({
+                        username: values.username,
+                        password: values.password
+                    })
+                }).then((response) => {
+                    message.success(response);
+                }).catch((error) => {
+                    message.error(error.responseText);
+                });
+            }
+        });
+    }
+    handleConfirmBlur = (e) => {
+        const value = e.target.value;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    }
+    checkPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Two passwords that you enter is inconsistent!');
+        } else {
+            callback();
+        }
+    }
+    checkConfirm = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    }
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 6 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 14 },
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 14,
+                    offset: 6,
+                },
+            },
+        };
+
+        return (
+            <Form onSubmit={this.handleSubmit} className="register-form">
+                <FormItem
+                    {...formItemLayout}
+                    label="Username"
+                    hasFeedback
+                >
+                    {getFieldDecorator('username', {
+                        rules: [{ required: true, message: 'Please input your username!'}],
+                    })(
+                        <Input />
+                    )}
+                </FormItem>
+                <FormItem
+                    {...formItemLayout}
+                    label="Password"
+                    hasFeedback
+                >
+                    {getFieldDecorator('password', {
+                        rules: [{
+                            required: true, message: 'Please input your password!',
+                        }, {
+                            validator: this.checkConfirm,
+                        }],
+                    })(
+                        <Input type="password" />
+                    )}
+                </FormItem>
+                <FormItem
+                    {...formItemLayout}
+                    label="Confirm Password"
+                    hasFeedback
+                >
+                    {getFieldDecorator('confirm', {
+                        rules: [{
+                            required: true, message: 'Please confirm your password!',
+                        }, {
+                            validator: this.checkPassword,
+                        }],
+                    })(
+                        <Input type="password" onBlur={this.handleConfirmBlur} />
+                    )}
+                </FormItem>
+                <FormItem {...tailFormItemLayout}>
+                    <Button type="primary" htmlType="submit">Register</Button>
+                </FormItem>
+            </Form>
+        );
+    }
+}
+
+export const WrappedRegister = Form.create()(RegistrationForm);
